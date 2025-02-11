3d5f0ad7339068696df736432837e897a26b730b
Samuel Smith <106135001+Weathersmith@users.noreply.github.com>
2025-02-11 06:18:35 -0700

    update ASOS
    adjusted size of entry fields, and added frames for the prepping METAR

--- ASOS.py
+++ ASOS.py
@@ -38,8 +38,8 @@ sky_label.grid(row=0, column=0)
 eq1 = tk.Label(data_frame, text="=", background='black', foreground='white')
 eq1.grid(row=0, column=1)
 
-sky_textbox = tk.Entry(data_frame, background='black', foreground='white', width=75)
-sky_textbox.grid(row=0, column=2, columnspan=11)
+sky_textbox = tk.Entry(data_frame, background='black', foreground='white')
+sky_textbox.grid(row=0, column=2)
 
 
 vis_label = tk.Label(data_frame, text="VISIBILITY", font=('Arial', 12), background='black', foreground='white')
@@ -57,25 +57,25 @@ temp_td_label.grid(row=1, column = 3)
 eq3 = tk.Label(data_frame, text="=", background='black', foreground='white')
 eq3.grid(row=1, column=4)
 
-tempc_textbox = tk.Entry(data_frame, background='black', foreground='white', width=5)
+tempc_textbox = tk.Entry(data_frame, background='black', foreground='white')
 tempc_textbox.grid(row=1, column=5)
 
 slash_label = tk.Label(data_frame, text="/", background='black', foreground='white')
 slash_label.grid(row=1, column=6)
 
-tdc_textbox = tk.Entry(data_frame, background='black', foreground='white', width=5)
+tdc_textbox = tk.Entry(data_frame, background='black', foreground='white')
 tdc_textbox.grid(row=1, column=7)
 
 unit_label_c = tk.Label(data_frame, text=" C ", font=('Arial', 12), background='black', foreground='white')
 unit_label_c.grid(row=1, column=8)
 
-tempf_textbox = tk.Entry(data_frame, background='black', foreground='white', width=5)
+tempf_textbox = tk.Entry(data_frame, background='black', foreground='white')
 tempf_textbox.grid(row=1, column=9)
 
 slash_label_2 = tk.Label(data_frame, text="/", background='black', foreground='white')
 slash_label_2.grid(row=1, column=10)
 
-tdf_textbox = tk.Entry(data_frame, background='black', foreground='white', width=5)
+tdf_textbox = tk.Entry(data_frame, background='black', foreground='white')
 tdf_textbox.grid(row=1, column=11)
 
 unit_label_f = tk.Label(data_frame, text=" F ", font=('Arial', 12), background='black', foreground='white')
@@ -97,7 +97,7 @@ eq5 = tk.Label(data_frame, text="=", background='black', foreground='white')
 eq5.grid(row=3, column=4)
 
 wind_textbox = tk.Entry(data_frame, background='black', foreground='white')
-wind_textbox.grid(row=3, column=5, columnspan=5)
+wind_textbox.grid(row=3, column=5)
 
 wx_label = tk.Label(data_frame, text="PRESENT WX", font=('Arial', 12), background='black', foreground='white')
 wx_label.grid(row=4, column=0)
@@ -116,6 +116,27 @@ eq6.grid(row=4, column=4)
 
 altmtr_textbox = tk.Entry(data_frame, background='black', foreground='white')
 altmtr_textbox.grid(row=4, column=5)
+
+report_prep_frame = tk.Frame(data_frame, background='black')
+report_prep_frame.grid(row=5, column=0, columnspan=10, rowspan=2)
+
+report_prep_label = tk.Label(report_prep_frame, font=('ARIAL', 12), text="METAR KSLC 18008KT 10SM FEW045 SCT090 BKN180 OVC260 08/M01 A2988 RMK AO2 SLPNO T00811012",
+                                 background='black', foreground='white', pady=10)
+report_prep_label.grid(column=0, row=0, sticky='w')
+
+def report_prep():
+    pass
+
+last_report_frame = tk.Frame(data_frame, background='black')
+last_report_frame.grid(row=6, column=0, columnspan=10, rowspan=2)
+
+last_report_label = tk.Label(report_prep_frame, font=('ARIAL', 12), text="METAR KSLC 17008KT 10SM FEW045 FEW090 SCT180 BKN260 07/00 A2988 RMK AO2 SLPNO T00741003",
+                                 background='black', foreground='white', pady=10)
+last_report_label.grid(column=0, row=1, sticky='w')
+
+checking_space = tk.Label(data_frame, text="bottom of last report frame")
+checking_space.grid(row=7, column=0)
+
 
 
 
